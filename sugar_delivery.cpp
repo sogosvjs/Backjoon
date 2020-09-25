be48972fae11e13661cb77abe53b45b928bd4a8d
dev-dongkyu <dongkyu.dev@gmail.com>
2020-09-25 12:35:37 +0900

    crushBak

--- sugar_delivery.cpp
+++ sugar_delivery.cpp
@@ -1,69 +1,189 @@
-//못푼문제.  나누려고만 했다. 빼기 생각못함. while문을 이용해서 if문 반복 사용
-/*
-#include <stdio.h>
+#include <Turboc.h>
 
-int main()
+
+
+#define MAX 1024
+
+#define LEFT 75
+
+#define RIGHT 77
+
+#define UP 72
+
+#define DOWN 80
+
+#define ESC 27
+
+
+
+struct Signal
+
 {
-	int N;
-	int quot_3, quot_5;
-	int rest_3, rest_5;
 
-	scanf("%d", &N);
+	BOOL exist;                // 신호의 존재 여부
 
-	if (N % 5 == 0)
-	{
-		quot_5 = N / 5;
-		printf("%d", quot_5);
-	}
-	else if (N % 3 == 0)
-	{
-		quot_3 = N / 3;
-		printf("%d", quot_3);
-	}
-	else
-	{
-		quot_5 = N / 5;
-		rest_5 = N % 5;
-		quot_3 = rest_5 / 3;
-		rest_3 = rest_5 % 3;
-		printf("%d", quot_3 + quot_5);
-	}
-}
-*/
+	char ch;                 // 출력할 문자
 
-#include <stdio.h>
-int main()
+	int x, y;                   // 현재 좌표
+
+	int distance;           // 이동할 거리
+
+	int nFrame;                 // 속도
+
+	int nStay;               // 속도에 대한 카운트
+
+};
+
+Signal S[MAX];
+
+
+
+void main()
+
 {
-	int input;
-	int cnt = 0;
 
-	scanf_s("%d", &input);
+	int i;
 
-	while (input > 0)
-	{
-		if (input % 5 == 0)
-		{
-			input -= 5;
-			cnt++;
+	int ch;
+
+	int freq = 15;
+
+	int frame = 200;
+
+
+
+	clrscr();
+
+	gotoxy(0, 23);
+
+	printf("매트릭스 흉내내기. 상하:속도 증감, 좌우:빈도 증감, ESC:종료");
+
+	randomize();
+
+	for (;;) {
+
+		gotoxy(60, 23);
+
+		printf("속도:%d, 빈도:%d    ", frame, freq);
+
+
+
+		// 키 입력 처리
+
+		if (kbhit()) {
+
+			ch = getch();
+
+			if (ch == 0xE0) {
+
+				ch = getch();
+
+				switch (ch) {
+
+				case UP:
+
+					frame = min(frame + 10, 1000);
+
+					break;
+
+				case DOWN:
+
+					frame = max(frame - 10, 10);
+
+					break;
+
+				case RIGHT:
+
+					freq = max(freq - 1, 1);
+
+					break;
+
+				case LEFT:
+
+					freq = min(freq + 1, 30);
+
+					break;
+
+				}
+
+			}
+			else {
+
+				if (ch == ESC) {
+
+					return;
+
+				}
+
+			}
+
 		}
-		else if (input % 3 == 0)
-		{
-			input -= 3;
-			cnt++;
+
+
+
+		// 새로운 신호 생성
+
+		if (random(freq) == 0) {
+
+			for (i = 0; i < MAX; i++) {
+
+				if (S[i].exist == FALSE) {
+
+					S[i].exist = TRUE;
+
+					S[i].ch = random('Z' - 'A' + 1) + 'A';
+
+					S[i].x = random(80);
+
+					S[i].y = 0;
+
+					S[i].distance = random(14) + 9;
+
+					S[i].nFrame = S[i].nStay = random(15) + 5;
+
+					break;
+
+				}
+
+			}
+
 		}
-		else if (input > 5)
-		{
-			input -= 5;
-			cnt++;
+
+
+
+		// 주기가 다 된 신호 이동 및 제거 처리
+
+		for (i = 0; i < MAX; i++) {
+
+			if (S[i].exist == FALSE)
+
+				continue;
+
+			if (--S[i].nStay == 0) {
+
+				S[i].nStay = S[i].nFrame;
+
+				gotoxy(S[i].x, S[i].y); putch(' ');
+
+				if (++S[i].y < S[i].distance) {
+
+					gotoxy(S[i].x, S[i].y); putch(S[i].ch);
+
+				}
+				else {
+
+					S[i].exist = FALSE;
+
+				}
+
+			}
+
 		}
-		else
-		{
-			printf("-1");				//cnt=-1;
-			break;						//break;
-		}
+
+
+
+		delay(1000 / frame);
+
 	}
-	if(input==0)						//위처럼 else문 작성 시 조건문 없애기 가능
-		printf("%d\n", cnt);
+
 }
-
-
